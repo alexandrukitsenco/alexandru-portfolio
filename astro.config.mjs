dd98c1c5aa625b1042c2f5523bce19c9328e7ae6
alexandrukitsenco <alexandru.kitsenco@gmail.com>
2024-01-24 22:04:07 +0100

    build: :rocket: add new packages and configure storyblok

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,30 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import vue from "@astrojs/vue";
-
+import storyblok from "@storyblok/astro";
+import { loadEnv } from "vite";
 import tailwind from "@astrojs/tailwind";
+import basicSsl from "@vitejs/plugin-basic-ssl";
 
-// https://astro.build/config
+const env = loadEnv("", process.cwd(), "STORYBLOK_TOKEN");
+
 export default defineConfig({
-  integrations: [vue(), tailwind()]
+  integrations: [
+    vue(),
+    tailwind(),
+    storyblok({
+      accessToken: env.STORYBLOK_TOKEN,
+      components: {
+        page: "storyblok/Page",
+        feature: "storyblok/Feature",
+        grid: "storyblok/Grid",
+        teaser: "storyblok/Teaser",
+      },
+    }),
+  ],
+  vite: {
+    plugins: [basicSsl()],
+    server: {
+      https: true,
+    },
+  },
 });
